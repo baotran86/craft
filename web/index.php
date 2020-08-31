dbebfb9cd078f8e3681b4baa2b2f8d4b9b3dba0c
Bao Tran <bao.tran@opswat.com>
2020-08-31 13:57:58 +0700

    Fix syntax error

--- web/index.php
+++ web/index.php
@@ -10,7 +10,7 @@ define('CRAFT_BASE_PATH', dirname(__DIR__));
 define('CRAFT_VENDOR_PATH', CRAFT_BASE_PATH.'/vendor');
 
 // Load Composer's autoloader
-require_once CRAFT_VENDOR_PATH.'/autoload.php';``
+require_once CRAFT_VENDOR_PATH.'/autoload.php';
 
 // Load dotenv?
 if (file_exists(CRAFT_BASE_PATH.'/.env')) {
